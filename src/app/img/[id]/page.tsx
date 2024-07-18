482f39b4e376eb05cf4124f70926a923a80794b9
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-07-18 23:45:57 +0530

    minor fix

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -5,10 +5,9 @@ export default async function PhotoPage({
 }: {
     params: { id: string };
 }) {
-    const idAsNumber = Number(photoId);
-    if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-
     return (
-        <FullPageImageView id={idAsNumber} />
+        <div className="flex h-full min-h-0 w-full min-w-0 overflow-y-hidden">
+            <FullPageImageView photoId={photoId} />
+        </div>
     );
 }
