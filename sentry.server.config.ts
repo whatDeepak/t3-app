0d4b9609e1ac82dd8f55b4c0a825a8b7d1ea9669
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-07-18 17:51:50 +0530

    Sentry and Parallel route

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a3efb7a7f886008852a4b3a46cd7db8d@o4507622402490368.ingest.us.sentry.io/4507622421823488",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 
@@ -13,12 +14,15 @@ async function Images() {
       <div className="flex flex-wrap justify-center gap-4 p-4">
         {images.map((image) => (
           <div key={image.id} className="w-48 h-48 flex flex-col">
-            <Image
-              src={image.url}
-              style={{ objectFit: "contain" }}
-              width={192}
-              height={192}
-              alt={image.name} />
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                style={{ objectFit: "contain" }}
+                width={192}
+                height={192}
+                alt={image.name}
+              />
+            </Link>
             <div>{image.name}</div>
           </div>
         ))}
